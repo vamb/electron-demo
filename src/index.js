b26c4d1238bfff03558730ea2dfa9f80fcf9d66b
zhangsy <zhangsy@think-land.com>
2023-12-04 16:54:27 +0800

    feat: min & max & close & check is maximized commit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
-const { BrowserWindow } = require('@electron/remote')
+const { BrowserWindow, getCurrentWindow } = require('@electron/remote')
 const path = require('path');
 
 window.addEventListener('DOMContentLoaded', ()=>{
-  console.log('11111')
+  // console.log('11111')
+
+  let mainWindow = getCurrentWindow()
 
   const midBtnDom = document.getElementById('min-btn')
   const maxBtnDom = document.getElementById('max-btn')
   const closeBtnDom = document.getElementById('close-btn')
-  console.log('midBtnDom maxBtnDom closeBtnDom', midBtnDom, maxBtnDom, closeBtnDom)
+  // console.log('midBtnDom maxBtnDom closeBtnDom', midBtnDom, maxBtnDom, closeBtnDom)
 
   midBtnDom?.addEventListener('click', ()=>{
-    console.log('minBtn Click')
+    // console.log('minBtn Click')
+    mainWindow.minimize()
   })
 
   maxBtnDom?.addEventListener('click', ()=>{
-    console.log('maxBtnDom click')
+    // console.log('maxBtnDom click isMaximized', mainWindow.isMaximized())
+    if(!mainWindow.isMaximized()) {
+      // console.log('11111')
+      mainWindow.maximize()
+    }else{
+      // console.log('22222')
+      mainWindow.restore()
+    }
   })
 
   closeBtnDom?.addEventListener('click', ()=>{
-    console.log('closeBtnDom click')
+    // console.log('closeBtnDom click')
+    mainWindow.close()
   })
 
   // 点击按钮打开一个新窗口
